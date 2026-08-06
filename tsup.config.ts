02bd84d2d1c51ad7a83d1fb2da2d5ccfa31ddc48
Tahmid Alam <tahmidulalam4@gmail.com>
2026-08-06 21:06:07 +0600

    Update tsup entry for Vercel

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/server.ts"],
+  entry: ["api/index.ts"],
   format: ["esm"],
   clean: true,
   sourcemap: true,
